410e1862bf05d685f5cdd478e5b10c8b539a1211
Lucas Moura <lucassiyahoo.com.br@gmail.com>
2022-03-20 14:19:00 -0300

    feat: port tweak

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const config = require('./config/systemConfiguration')
 const app = config()
 const PORT = process.env.PORT
 
-app.listen(PORT, (err) => {
+app.listen(PORT || 3000, (err) => {
   err
     ? console.log(err)
     : console.log(`Server running on port ${PORT}`)
