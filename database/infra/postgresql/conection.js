ac9084c631567513af38dddada040590a1f9932e
Lucas Moura <lucassiyahoo.com.br@gmail.com>
2022-03-20 14:04:33 -0300

    feat: SSL config added to connection

--- database/infra/postgresql/conection.js
+++ database/infra/postgresql/conection.js
@@ -1,13 +1,18 @@
 require('dotenv').config()
 const { Pool: Conection } = require('pg')
-
-module.exports = new Conection({
+const SSL = process.env.SSL_ENABLE
+const config = {
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_BASE,
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
-  ssl: {
+}
+
+if(SSL) {
+  config.ssl = {
     rejectUnauthorized: false
   }
-})
+}
+
+module.exports = new Conection(config)
