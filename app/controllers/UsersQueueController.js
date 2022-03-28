9d0ebbe5de69f373ec6410b5182def23f6384af6
lucas.moura <lucas.moura@meta.com.br>
2022-03-28 15:21:41 -0300

    feat: update queue func added

--- app/controllers/UsersQueueController.js
+++ app/controllers/UsersQueueController.js
@@ -60,6 +60,25 @@ class UsersQueueController extends Controller {
       }
     })
 
+    this.app.route( this.getRoute( 'updateUserQueue' ) ).patch(async (request, response) => {
+      try {
+        const { params, body: payload } = request
+        await this.Model.update({
+          ...payload,
+          updated_at: new Date().toLocaleDateString()
+        }, params)
+                
+        return response
+          .status(200)
+          .json({status: 200, data: 'Data row successfully updated'})
+      } catch(error) {
+        console.error(error.message)
+        return response
+          .status(500)
+          .json({status: 500, data: error.message})
+      }
+    })
+
     this.overrides(this.getRoute('store'))
     this.app.route(this.getRoute('store')).post(async (request, response) => {
       try {
